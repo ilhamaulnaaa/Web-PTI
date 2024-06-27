2efa35a35c8672f3f83d65b2cd162c653aeeb4ec
Ilham Maulana <Ilhamaulnaaa@gmail.com>
2024-06-27 23:40:49 +0700

    First Commit

---
+++ js/main.js
@@ -0,0 +1,13 @@
+document.getElementById("wishlistButton").addEventListener("click", function () {
+  window.location.href = "wishlist.html";
+});
+
+document.getElementById("cartButton").addEventListener("click", function () {
+  window.location.href = "cart.html";
+});
+document.getElementById("SignUpButton").addEventListener("click", function () {
+  window.location.href = "forms.html";
+});
+document.getElementById("SignUpConfirmation").addEventListener("click", function () {
+  window.location.href = "Sparkblock.html";
+});
